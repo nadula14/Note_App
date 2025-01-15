d32432d2139b459a9793e62e60ae80b60f3cb371
nadula14 <madugalle_ewmwwndb_e22@engug.ruh.ac.lk>
2025-01-16 01:03:45 +0530

    third

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -46,13 +46,16 @@ class RouterClass {
         GoRoute(
           path: "/user",
           builder: (context, state) {
-            final String name = state.extra as String;
-            final int age = state.extra as int;
-            
+            final name =
+                (state.extra as Map<String, dynamic>)
+                ["name"] as String;
+
+            final age = (state.extra as Map<String, dynamic>)
+                ["age"] as int;
             return UserPage(
               userName: name,
               userAge: age,
-              );
+            );
           },
         ),
       ]);
